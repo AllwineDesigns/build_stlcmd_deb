8552ccd7975b36366e3bf4466f126d7f1135a4a6
John Allwine <jallwine86@gmail.com>
2017-12-17 23:41:39 -0700

    fixed some lintian warnings

--- build_stlcmd.sh
+++ build_stlcmd.sh
@@ -22,14 +22,15 @@ stlcmd (1.0-1) UNRELEASED; urgency=low
 
 EOF
 
-echo 10 > debian/compat
+echo 9 > debian/compat
 
 cat <<EOF > debian/control
 Source: stlcmd
 Maintainer: John Allwine <john@allwinedesigns.com>
+Homepage: https://www.github.com/AllwineDesigns/stl_cmd
 Section: misc
 Priority: optional
-Standards-Version: 3.9.7
+Standards-Version: 3.9.8
 Build-Depends: debhelper (>= 9)
 
 Package: stlcmd
@@ -48,7 +49,34 @@ Description: Suite of commands for generating, inspecting and manipulating STL f
 EOF
 
 cat <<EOF > debian/copyright
-Copyright Allwine Designs, LLC 2017
+Format: https://www.debian.org/doc/packaging-manuals/copyright-format/1.0/
+Upstream-Name: stl_cmd
+Upstream-Contact: John Allwine <john@allwinedesigns.com>
+Source: https://www.github.com/AllwineDesigns/stl_cmd
+License: GPL-3
+Copyright: 2017 Allwine Designs, LLC
+
+Files: *
+Copyright: 2017 Allwine Designs, LLC
+License: GPL-3
+
+License: GPL-3
+  This package is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+  .
+  This package is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  .
+  You should have received a copy of the GNU General Public License
+  along with this package; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+  .
+  On Debian systems, the complete text of the GNU General
+  Public License can be found in \`/usr/share/common-licenses/GPL-3'.
 EOF
 
 cat <<EOF > debian/rules
